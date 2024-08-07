4f66f030c710fb9809ce1b2ce4823df701eaa0e7
Anonymous3017 <ayushgupta9592@gmail.com>
2024-08-07 16:04:41 +0530

    updated

--- demo.dart
+++ demo.dart
@@ -1,10 +1,9 @@
 void main() {
-
   var name = "Ayush";
   int age = 20;
-  print('so the person name is $name and its age is $age'); 
-  dynamic first =12;
+  print('so the person name is $name and its age is $age');
+  dynamic first = 12;
   dynamic second = 45.5;
-print(first + second);
-  
+  print(first + second);
+  print('the quick brown fox jumps over the lazy dog');
 }
