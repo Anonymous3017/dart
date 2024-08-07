70b6e20b3e3aa3aac2de95a30ea1b7707d723575
Anonymous3017 <ayushgupta9592@gmail.com>
2024-08-07 16:05:59 +0530

    practice loops

---
+++ loops.dart
@@ -0,0 +1,23 @@
+void main() {
+  // for loop
+  for (int i = 1; i<6; i++) {
+    print('this iteration is $i');
+  }
+  
+  
+  // while loop
+  
+  int j = 1;
+  while (j < 6) {
+    print('this ittration in while loop is $j');
+    j++;
+  }
+  
+
+  // do-while loop
+  int k = 1;
+  do {
+    print('This ittration in do-while loop us $k');
+    k++;
+  } while (k < 6);
+}
