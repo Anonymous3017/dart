16c34574f796cf70bc7b28cd53fb93665d6a063b
Anonymous3017 <ayushgupta9592@gmail.com>
2024-08-07 16:03:44 +0530

    practice conditionals

---
+++ if-else-statements.dart
@@ -0,0 +1,11 @@
+void main() {
+  int num = 0;
+
+  if (num > 0) {
+    print('poditive hai');
+  } else if(num < 0 ) {
+    print('negative hai');
+  } else {
+    print('garam hai, garam hai');
+  }
+}
