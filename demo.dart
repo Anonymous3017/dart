3f1d5addd8bab2abefb033b64d4a6e403b355cd7
Anonymous3017 <ayushgupta9592@gmail.com>
2024-08-07 15:30:02 +0530

    first dart code

---
+++ demo.dart
@@ -0,0 +1,10 @@
+void main() {
+
+  var name = "Ayush";
+  int age = 20;
+  print('so the person name is $name and its age is $age'); 
+  dynamic first =12;
+  dynamic second = 45.5;
+print(first + second);
+  
+}
