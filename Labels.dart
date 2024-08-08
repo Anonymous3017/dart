018953be564d8b5ad4e7dcb094c27e61a52dde41
Anonymous3017 <ayushgupta9592@gmail.com>
2024-08-08 17:24:34 +0530

    concept of Labels in loops

---
+++ Labels.dart
@@ -0,0 +1,37 @@
+void main() {
+  // Labels in Dart
+  // You can label a loop in Dart using the label: identifier syntax.
+  // The label can be used with break and continue statements to control the flow of the loop.
+
+  // Syntax
+  // label: for (var i = 0; i < 5; i++) {
+  //   // Statements
+  // }
+  /*
+
+  outerLoop: for (var i = 0; i < 5; i++) {
+    innerLoop: for (var j = 0; j < 5; j++) {
+      if (i == 2 && j == 2) {
+        break outerLoop;
+      }
+      print('i = $i, j = $j');
+    }
+  }
+  */
+
+  // The break statement with a label breaks out of the loop with the specified label.
+  // In the above example, the break outerLoop statement breaks out of the outer loop when i is 2 and j is 2.
+
+  //some more examples
+  outerLoop: for (var i = 0; i < 5; i++) {
+    innerLoop: for (var j = 0; j < 5; j++) {
+      if (i == 2 && j == 2) {
+        continue outerLoop;
+      }
+      print('i = $i, j = $j');
+    }
+  }
+
+  // The continue statement with a label skips the current iteration of the loop with the specified label.
+  // In the above example, the continue outerLoop statement skips the current iteration of the outer loop when i is 2 and j is 2.
+}
--- tempCodeRunnerFile.dart
+++ tempCodeRunnerFile.dart
@@ -1,2 +1,8 @@
-
-  // print('\nList after removing even elements is $numbers');
+  // outerLoop: for (var i = 0; i < 5; i++) {
+  //   innerLoop: for (var j = 0; j < 5; j++) {
+  //     if (i == 2 && j == 2) {
+  //       break outerLoop;
+  //     }
+  //     print('i = $i, j = $j');
+  //   }
+  // }
