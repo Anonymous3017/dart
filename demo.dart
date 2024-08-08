807c5085263c9c7483ecb98985054f7e099e87d0
Anonymous3017 <ayushgupta9592@gmail.com>
2024-08-08 16:01:35 +0530

    update the concept of final and const keyword

--- demo.dart
+++ demo.dart
@@ -6,4 +6,20 @@ void main() {
   dynamic second = 45.5;
   print(first + second);
   print('the quick brown fox jumps over the lazy dog');
+
+  // Final keyword without datatype in dart
+  final firstName = "Ayush";
+  // name1 = "Ayush Gupta"; // This will give an error
+  final String lastname = "Gupta";
+  print('The name of the person is $firstName $lastname');
+
+  // Const keyword in dart
+
+}
+
+class student {
+  final String name = "Ayush";
+  // const int age = 20; // This will give an error you have to set static keyword
+  static const int age = 20;
+  
 }
