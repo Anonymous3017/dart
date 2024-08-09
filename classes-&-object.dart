e8f3d9b9d2b99d0787d1f954c8c1ca81df2571c4
Anonymous3017 <ayushgupta9592@gmail.com>
2024-08-09 15:03:53 +0530

    making your own custom constructor

--- classes-&-object.dart
+++ classes-&-object.dart
@@ -1,5 +1,5 @@
 void main() {
-  var std1 = Student();
+  var std1 = Student.customConstructor();
   std1.id = 1;
   std1.name = 'Ayush';
   print('ID: ${std1.id} \nName: ${std1.name}');
@@ -7,28 +7,42 @@ void main() {
   std1.sleep();
   std1.eat();
 
-  var std2 = Student();
-  std2.id = 2;
-  std2.name = 'Mohit';
+  var std2 = Student.customConstructor();
   print('ID: ${std2.id} \nName: ${std2.name}');
   std2.study();
   std2.sleep();
   std2.eat();
-
 }
 
-//Defining a class
+// Defining a class
 class Student {
-  var id = null;
-  var name = null;
+  var id;
+  var name;
+
+  // Default constructor
+  // Student() {
+  //   print('This is a default constructor');
+  //   this.id = 0;
+  //   this.name = 'Unknown';
+  // }
+
+  // Parameterized constructor
+  // Student(this.id, this.name);
+
+  // Custom named constructor
+  Student.customConstructor() {
+    print('This is a custom constructor');
+  }
 
   void study() {
     print('${this.name} is studying');
   }
+
   void sleep() {
     print('${this.name} is sleeping');
   }
-  void eat() { 
+
+  void eat() {
     print('${this.name} is eating');
   }
 }
