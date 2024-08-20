aefc92238e3b071496fbc2fc6cacc18cc89f8598
Anonymous3017 <ayushgupta9592@gmail.com>
2024-08-20 13:37:03 +0530

    feat: Add drawer to main screen for navigation and BottomSheet for adding task

--- Flutter/todoapp/lib/mainScreen.dart
+++ Flutter/todoapp/lib/mainScreen.dart
@@ -11,15 +11,27 @@ class _MainscreenState extends State<Mainscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
       appBar: AppBar(
         title: Text('Todo App'),
         centerTitle: true,
         actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.add,
-              size: 30,
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return Container(
+                      height: 250,
+                    );
+                  });
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.add,
+                size: 30,
+              ),
             ),
           ),
         ],
