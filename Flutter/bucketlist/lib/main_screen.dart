56dd8b67c8998ebb6fa413744a5663ed09d11d78
Anonymous3017 <ayushgupta9592@gmail.com>
2024-08-22 18:27:37 +0530

    New Project BucketList used GET req to fetch API Data

---
+++ Flutter/bucketlist/lib/main_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  void getData() {
+    //Get Data from API using dio
+    Dio()
+        .get(
+            'https://flutterapitest12122002-default-rtdb.firebaseio.com/bucketlist.json')
+        .then((response) {
+      print(response.data);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bucket List'),
+      ),
+      body: ElevatedButton(
+        onPressed: getData,
+        child: const Text('Get Data'),
+      ),
+    );
+  }
+}
