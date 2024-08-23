e7c391d1a6aac0419be9350ea713e98f42c563a4
Anonymous3017 <ayushgupta9592@gmail.com>
2024-08-23 15:34:58 +0530

    feat: Add popup menu for completing and deleting items in ViewItemScreen

--- Flutter/bucketlist/lib/view_item.dart
+++ Flutter/bucketlist/lib/view_item.dart
@@ -20,6 +20,48 @@ class _ViewItemScreenState extends State<ViewItemScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          PopupMenuButton(onSelected: (value) {
+            if (value == 'complete') {
+              // Mark the item as complete
+            } else if (value == 'delete') {
+              // Delete the item
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text('Delete item'),
+                      content: const Text(
+                          'Are you sure you want to delete this item?'),
+                      actions: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancel')),
+                        TextButton(
+                            onPressed: () {
+                              // Delete the item
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Delete')),
+                      ],
+                    );
+                  });
+            }
+          }, itemBuilder: (context) {
+            return [
+              const PopupMenuItem(
+                value: 'complete',
+                child: Text('Mark as complete'),
+              ),
+              const PopupMenuItem(
+                value: 'delete',
+                child: Text('Delete'),
+              ),
+            ];
+          })
+        ],
       ),
       body: Column(
         children: [
