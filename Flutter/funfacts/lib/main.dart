9b64a7f37323528842fec4f74c13eaac9c574881
Anonymous3017 <ayushgupta9592@gmail.com>
2024-08-24 23:03:43 +0530

    ```text feat: Save theme mode to shared preferences
    Save the selected theme mode (light or dark) to shared preferences when the theme is updated in the theme switcher widget. This ensures that the selected theme persists across app restarts.

--- Flutter/funfacts/lib/main.dart
+++ Flutter/funfacts/lib/main.dart
@@ -17,6 +17,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
+  void initState() {
+    Provider.of<ThemeProvider>(context, listen: false).getThemeMode();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     var themeProvider = Provider.of<ThemeProvider>(context);
     return MaterialApp(
--- Flutter/funfacts/lib/providers/theme_provider.dart
+++ Flutter/funfacts/lib/providers/theme_provider.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   bool isDarkModeChecked = true;
 
-  void updateTheme({required bool darkMode}) {
+  void updateTheme({required bool darkMode}) async {
     isDarkModeChecked = darkMode;
+
+    //Obtain share4d preferences and save the theme mode
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    //Save the theme mode to shared preferences
+    prefs.setBool('darkMode', darkMode);
+    notifyListeners();
+  }
+
+  void getThemeMode() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    isDarkModeChecked = prefs.getBool('darkMode') ?? true;
     notifyListeners();
   }
 }
