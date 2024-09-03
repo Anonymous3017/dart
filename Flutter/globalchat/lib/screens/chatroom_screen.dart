e4eac2058d59deff62105036804d07173f28ab08
Anonymous3017 <ayushgupta9592@gmail.com>
2024-09-03 13:46:00 +0530

    feat: implement sendMessage func to send user msg to database, and some UI corre4ctions

--- Flutter/globalchat/lib/screens/chatroom_screen.dart
+++ Flutter/globalchat/lib/screens/chatroom_screen.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:globalchat/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
 class ChatroomScreen extends StatefulWidget {
   String chatroomName;
@@ -14,6 +17,30 @@ class ChatroomScreen extends StatefulWidget {
 }
 
 class _ChatroomScreenState extends State<ChatroomScreen> {
+  var db = FirebaseFirestore.instance;
+  TextEditingController messageText = TextEditingController();
+
+  Future<void> sendMessage() async {
+    //validation
+    if (messageText.text.isEmpty) {
+      return;
+    }
+    // send message to chatroom
+    Map<String, dynamic> messageToSend = {
+      "text": messageText.text,
+      "sender_name": Provider.of<UserProvider>(context, listen: false).userName,
+      "chatroom_id": widget.chatroomId,
+      "timestamp": FieldValue.serverTimestamp()
+    };
+
+    try {
+      await db.collection("messages").add(messageToSend);
+    } on Exception catch (e) {
+      // TODO
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,15 +53,29 @@ class _ChatroomScreenState extends State<ChatroomScreen> {
           children: [
             Expanded(
                 child: Container(
-              color: Colors.red,
+              color: Colors.white,
             )),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(child: const TextField()),
-                  const Icon(Icons.send),
-                ],
+            Container(
+              color: Colors.grey[200],
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                      controller: messageText,
+                      decoration: const InputDecoration(
+                          border: InputBorder.none, hintText: "Type a message"),
+                    )),
+                    InkWell(
+                      child: const Icon(Icons.send),
+                      onTap: () {
+                        sendMessage();
+                        messageText.clear();
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
